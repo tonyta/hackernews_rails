f72e4df68e5e04b7cce6eb754ba734b25cc7716b
Apprentice <apprentice@dbc-chicago-001.local>
2014-03-31 14:05:06 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNews::Application.config.secret_key_base = '305f22cad4e073228a5dc933dea2bf8e86f908d6cb41145e36078a192e353a0cbf531509d634c76a766f5776adbd9f956af9d0dc6332e4a232c42dd1388c29c3'
